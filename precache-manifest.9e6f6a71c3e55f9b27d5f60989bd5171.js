3497b19e8fa4e9e3d3dbca711be7db189797c0b1
Fandrelo <14915329+Fandrelo@users.noreply.github.com>
2020-02-04 21:54:02 -0600

    Updates

---
+++ precache-manifest.9e6f6a71c3e55f9b27d5f60989bd5171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752c3d2b0a0872f9e6010c2a5804315d",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "3f2d854be3c8ee0055a6",
+    "url": "/Portafolio/static/css/main.841c46e5.chunk.css"
+  },
+  {
+    "revision": "fb7b17c6f1cd747354b9",
+    "url": "/Portafolio/static/js/2.2183980d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Portafolio/static/js/2.2183980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2d854be3c8ee0055a6",
+    "url": "/Portafolio/static/js/main.519fe220.chunk.js"
+  },
+  {
+    "revision": "8931cc960d4f9a59f192",
+    "url": "/Portafolio/static/js/runtime-main.26a8cb9b.js"
+  },
+  {
+    "revision": "eae0a375eb2c42eeb86b516d91364031",
+    "url": "/Portafolio/static/media/logo.eae0a375.png"
+  }
+]);
